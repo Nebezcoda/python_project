48ec87c85bce4630e755046cccc452b8584dd8a2
Alexander Volkov <nebezcoda@gmail.com>
2023-08-23 14:36:12 +0300

    --

--- 1-file.py
+++ 1-file.py
@@ -1,21 +1,4 @@
-from dataclasses import dataclass
+def get_string(string: str, number: int) -> str:
+    return string * number
 
-
-@dataclass
-class DatabaseConfig:
-    db_host: str       # URL-адрес базы данных
-    db_user: str       # Username пользователя базы данных
-    db_password: str   # Пароль к базе данных
-    database: str      # Название базы данных
-
-
-@dataclass
-class TgBot:
-    token: str             # Токен для доступа к телеграм-боту
-    admin_ids: list[int]   # Список id администраторов бота
-
-
-@dataclass
-class Config:
-    tg_bot: TgBot
-    db: DatabaseConfig
+print(get_string.__annotations__)
