9e5533429af5a6a6bd3f3bf4029d67ed19cc458d
Alexander Volkov <nebezcoda@gmail.com>
2023-09-01 11:47:41 +0300

    -

--- Эхо-бот-2.py
+++ Эхо-бот-2.py
@@ -27,6 +27,36 @@ async def send_photo_echo(message: Message):
     await message.reply_photo(message.photo[0].file_id)
 
 
+# Этот хэндлер будет срабатывать на отправку видео
+async def send_video_echo(message: Message):
+    await message.reply_video(message.video.file_id)
+
+
+# Этот хэндлер будет срабатывать на отправку видео сообщений
+async def send_video_note_echo(message: Message):
+    await message.reply_video_note(message.video_note.file_id)
+
+
+# Этот хэндлер будет срабатывать на отправку стикеров
+async def send_sticker_echo(message: Message):
+    await message.reply_sticker(message.sticker.file_id)
+
+
+# Этот хэндлер будет срабатывать на отправку аудио файлов
+async def send_audio_echo(message: Message):
+    await message.reply_audio(message.audio.file_id)
+
+
+# Этот хэндлер будет срабатывать на отправку голосовых сообщений
+async def send_voice_echo(message: Message):
+    await message.reply_voice(message.voice.file_id)
+
+
+# Этот хэндлер будет срабатывать на отправку любых файлов
+async def send_document_echo(message: Message):
+    await message.reply_document(message.document.file_id)
+
+
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
 async def send_echo(message: Message):
@@ -37,6 +67,14 @@ async def send_echo(message: Message):
 dp.message.register(process_start_command, Command(commands=["start"]))
 dp.message.register(process_help_command, Command(commands=['help']))
 dp.message.register(send_photo_echo, F.photo)
+
+dp.message.register(send_video_echo, F.video)
+dp.message.register(send_video_note_echo, F.video_note)
+dp.message.register(send_sticker_echo, F.sticker)
+dp.message.register(send_audio_echo, F.audio)
+dp.message.register(send_voice_echo, F.voice)
+dp.message.register(send_document_echo, F.document)
+
 dp.message.register(send_echo)
 
 if __name__ == '__main__':
