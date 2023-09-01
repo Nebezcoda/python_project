62a4eea4ccdbfd2bc8eda10b81458918dee06f81
Alexander Volkov <nebezcoda@gmail.com>
2023-09-01 11:27:17 +0300

    --

--- Эхо-бот-2.py
+++ Эхо-бот-2.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
 
@@ -22,6 +22,11 @@ async def process_help_command(message: Message):
                          'я пришлю тебе твое сообщение')
 
 
+# Этот хэндлер будет срабатывать на отправку боту фото
+async def send_photo_echo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
+
+
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
 async def send_echo(message: Message):
@@ -31,6 +36,7 @@ async def send_echo(message: Message):
 # Регистрируем хэндлеры
 dp.message.register(process_start_command, Command(commands=["start"]))
 dp.message.register(process_help_command, Command(commands=['help']))
+dp.message.register(send_photo_echo, F.photo)
 dp.message.register(send_echo)
 
 if __name__ == '__main__':
