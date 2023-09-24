60214693a546502a4a20626f4c8410a00897265f
Alexander Volkov <nebezcoda@gmail.com>
2023-09-24 14:18:01 +0300

    +

--- Модули/Packs/main.py
+++ Модули/Packs/main.py
@@ -9,16 +9,16 @@
 # print('b =', b)
 
 
-print('Это основной модуль main.py, его имя в процессе выполнения программы:', __name__)
+# print('Это основной модуль main.py, его имя в процессе выполнения программы:', __name__)
 
 
-from pack_1 import file_11
-from pack_2.pack_21 import file_211
+# from pack_1 import file_11
+# from pack_2.pack_21 import file_211
 
 
-print('a =', file_11.a)
-print('b =', file_211.b)
-print('Словарь some_dict:', file_211.some_dict)
+# print('a =', file_11.a)
+# print('b =', file_211.b)
+# print('Словарь some_dict:', file_211.some_dict)
 
 
 # print('Это основной модуль main.py, его имя в процессе выполнения программы:', __name__)
@@ -28,3 +28,13 @@ print('Словарь some_dict:', file_211.some_dict)
 
 
 # print('b =', pack_21.file_211.b)
+
+
+
+print('Это основной файл main.py, его имя в процессе выполнения программы:', __name__)
+
+
+from pack_1.file_11 import result
+
+
+print('result =', result)
